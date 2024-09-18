12f9ecb381355fc0cf39638842b8b3a501550165
Gianlucabernasconi <gianlucabernasconi@protonmail.com>
2024-09-18 20:43:58 -0300

    1

--- server.js
+++ server.js
@@ -4,28 +4,27 @@ import client from './src/common/db.js'
 import actorRoutes from './src/actor/routes.js'
 import peliculaRoutes from './src/pelicula/routes.js'
 
-
-const PORTS = 3000 || 4000
+const PORTS = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
 app.use(cors())
 
-app.all('/', (req, res) => { return res.status(200).send('Bienvenido al Cine!')})
+app.all('/', (req, res) => { 
+    return res.status(200).send('Bienvenido al Cine!')
+})
 
 app.use('/api', actorRoutes)
 app.use('/api', peliculaRoutes)
 
-
-
 await client.connect()
 .then(() => {
     console.log('Conectado al cluster')
-    app.listen(PORTS, () => { console.log(`Servidor corriendo en http://localhost:${PORTS}` ) })
+    app.listen(PORTS, () => { 
+        console.log(`Servidor corriendo en http://localhost:${PORTS}`) 
+    })
 })
-.catch(() => {
-    console.log('Error al conectarse con la db')
-    
+.catch((error) => {
+    console.log('Error al conectarse con la db:', error)
 })
-
