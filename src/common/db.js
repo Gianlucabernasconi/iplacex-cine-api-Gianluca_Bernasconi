bbf9d0e3c7b3144828c0999401b38b841649082e
Gianlucabernasconi <gianlucabernasconi@protonmail.com>
2024-09-18 20:48:39 -0300

    Update db.js

--- src/common/db.js
+++ src/common/db.js
@@ -1,6 +1,8 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = 'mongodb+srv://gian:iplacex123@cine-db.cse0m.mongodb.net/?retryWrites=true&w=majority&appName=cine-db'
+const uri = 'mongodb+srv://gian:iplacex123@cine-db.cse0m.mongodb.net/?retryWrites=true&w=majority&appName=cine-db&ssl=false';
+
+
 
 const client = new MongoClient(uri, {
     ServerApi:{
